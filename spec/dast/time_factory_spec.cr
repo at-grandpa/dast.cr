ad3acbd98dfadaede139bc7b7c2ea8aec15cceb7
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-18 18:29:22 +0900

    ...

--- spec/dast/time_factory_spec.cr
+++ spec/dast/time_factory_spec.cr
@@ -8,7 +8,7 @@ describe Dast::TimeFactory do
           arguments: ["2018-03-18", "2018-03-19"],
           expect:    {
             from: Time.parse("2018-03-18", "%F"),
-            to: Time.parse("2018-03-19", "%F"),
+            to:   Time.parse("2018-03-19", "%F"),
           },
         },
       ].each do |spec_case|
@@ -16,6 +16,37 @@ describe Dast::TimeFactory do
           from, to = Dast::TimeFactory.new(arguments: spec_case[:arguments]).create_time_from_and_to
           from.should eq spec_case[:expect][:from]
           to.should eq spec_case[:expect][:to]
+        end
+      end
+    end
+    describe "raises an Exception, " do
+      [
+        {
+          arguments: [] of String,
+        },
+        {
+          arguments: ["2018-03-18", "2018-03-19", "2018-03-20"],
+        },
+      ].each do |spec_case|
+        it "when #{spec_case.to_h.reject { |k, _| k.to_s == "expect" }}" do
+          expect_raises(Exception, "Wrong number of arguments. (given #{spec_case[:arguments].size}, expected 2)") do
+            Dast::TimeFactory.new(arguments: spec_case[:arguments]).create_time_from_and_to
+          end
+        end
+      end
+    end
+  end
+  describe "#format_for_time" do
+    describe "returns string for Time.parse, " do
+      [
+        {
+          input:  "2018-03-18",
+          expect: "2018-03-18",
+        },
+      ].each do |spec_case|
+        it "when #{spec_case.to_h.reject { |k, _| k.to_s == "expect" }}" do
+          output = Dast::TimeFactory.new([] of String).format_for_time(spec_case[:input])
+          output.should eq spec_case[:expect]
         end
       end
     end
--- src/dast/time_factory.cr
+++ src/dast/time_factory.cr
@@ -4,7 +4,12 @@ module Dast
     end
 
     def create_time_from_and_to
+      raise Exception.new("Wrong number of arguments. (given #{@arguments.size}, expected 2)") unless @arguments.size == 2
       return Time.parse("2018-03-18", "%F"), Time.parse("2018-03-19", "%F")
+    end
+
+    def format_for_time(input)
+      input
     end
   end
 end
